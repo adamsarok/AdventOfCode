434aaeca63704ed2981557b7352dda463bdc8426
Ádám Sarok <sarok.adam@gmail.com>
2024-11-01 19:03:33 +0100

    day12

--- Day12.cs
+++ Day12.cs
@@ -1,85 +1,121 @@
 using System;
+using System.Diagnostics;
 
 namespace AdventOfCode;
 
 public class Day12 {
     public static void SolvePart1() {
         var lines = File.ReadAllLines("testinput.txt");
-        foreach (var line in lines) {
-            SolveLine(line.Split(' '));
+        int result = 0;
+        Stopwatch sw = Stopwatch.StartNew();
+        // Parallel.ForEach(lines, line => {
+        //     var solver = new Solver(line.Split(' '), false);
+        //     result += solver.Solutions.Count;
+        // });
+        foreach (var line in lines) { //current best is 1590ms 
+            var solver = new Solver(line.Split(' '), false);
+            result += solver.Solutions.Count;
         }
-        foreach (var s in solutions) {
-            Console.WriteLine(s);
-        }
-        Console.WriteLine(solutions.Count);
-        //TODO: works on test input but fails on real input :(
-        //Console.WriteLine(CheckLine("?#?###?###?####", new List<int>() { 1,3,1,6 }));
-    }
-    private static List<int> GetConditions(string str) {
-        List<int> conditions = new List<int>();
-        str.Split(',').ToList().ForEach(x => conditions.Add(int.Parse(x.Trim())));
-        return conditions;
-    }
+        sw.Stop();
+        Console.WriteLine($"Part1 elapsed: {sw.ElapsedMilliseconds}");
 
-    private static void SolveLine(string[] inputs) {
-        string line = inputs[0];
-        var conditions = GetConditions(inputs[1]);
-        SolveLine(line, 0, conditions);
+        //day2 will never finish with brute force approach 
+        // Stopwatch sw2 = Stopwatch.StartNew();
+        // Parallel.ForEach(lines, line => {
+        //     var solver = new Solver(line.Split(' '), true);
+        //     result += solver.Solutions.Count;
+        // });
+        // sw2.Stop();
+        // Console.WriteLine($"Part2 elapsed: {sw2.ElapsedMilliseconds}");
+        Console.WriteLine(result);
     }
-    static HashSet<string> solutions = new HashSet<string>();
-    private static void SolveLine(string input, int startFrom, List<int> conditions) {
-        if (startFrom >= input.Length) return;
-        SolveLine(input, startFrom + 1, conditions);
-        var next = input;
-        bool placed = false;
-        for (int i = startFrom; i < input.Length; i++) {
-            if (next[i] == '?') {
-                var arr = input.ToCharArray();
-                arr[i] = '#';
-                next = new string(arr);
-                placed = true;
-                break;
+    public class Solver {
+        public HashSet<string> Solutions = new HashSet<string>();
+        List<int> conditions;
+        string line;
+        public Solver(string[] inputs, bool isPart2) {
+            line = inputs[0];
+            conditions = GetConditions(inputs[1]);
+            if (isPart2) {
+                line = String.Concat(Enumerable.Repeat(line, 5));
+                for (int i = 0; i < 5; i++) conditions.AddRange(conditions);;
+            }
+            Solve();
+        }
+        private List<int> GetConditions(string str) {
+            List<int> conditions = new List<int>();
+            str.Split(',').ToList().ForEach(x => conditions.Add(int.Parse(x.Trim())));
+            return conditions;
+        }
+
+        private void Solve() {
+            switch (CheckLine(line, conditions)) {
+                case Conditions.Underflow:
+                    SolveFrom(line, 0, conditions);
+                    break;
+                case Conditions.Satisfied:
+                    Solutions.Add(line); //already complete input
+                    break;
             }
         }
-        //if (!placed) return;
-        var res = CheckLine(next, conditions);
-        //Console.WriteLine($" : {res}");
-        switch (res) {
-            case Conditions.Satisfied:
-                solutions.Add(next);
-                return;
-            case Conditions.Overflow:
-                return;
-            case Conditions.Underflow:
-                SolveLine(next, startFrom + 1, conditions);
-                break;
-        }
-    }
 
-    private enum Conditions { Satisfied, Overflow, Underflow }
-    private static Conditions CheckLine(string line, List<int> conditions) {
-        //Console.Write($"Checking:{line}, {string.Join(",", conditions)} ");
-        int actCond = 0;
-        int actSprings = 0;
-        int totalSprings = 0;
-        for (int i = 0; i < line.Length; i++) {
-            if (line[i] == '#') {
-                actSprings++;
-                totalSprings++;
-            }
-            else {
-                if (actSprings > 0) {
-                    if (actCond < conditions.Count && actSprings < conditions[actCond]) return Conditions.Underflow;
-                    actSprings = 0;
-                    actCond++;
+        private void SolveFrom(string input, int startFrom, List<int> conditions) {
+            if (startFrom >= input.Length) return;
+            SolveFrom(input, startFrom + 1, conditions);
+            var next = input;
+            for (int i = startFrom; i < input.Length; i++) {
+                if (next[i] == '?') {
+                    var arr = input.ToCharArray();
+                    arr[i] = '#';
+                    next = new string(arr);
+                    break;
                 }
             }
+            if (checkedAlready.Contains(next)) return;
+            var res = CheckLine(next, conditions);
+            checkedAlready.Add(next);
+            //Console.WriteLine($" : {res}");
+            switch (res) {
+                case Conditions.Satisfied:
+                    //Console.WriteLine(next);
+    
+                    Solutions.Add(next);
+                    return;
+                case Conditions.Overflow:
+                    return;
+                case Conditions.Underflow:
+                    SolveFrom(next, startFrom + 1, conditions);
+                    break;
+            }
         }
-        if (totalSprings > conditions.Sum()) return Conditions.Overflow;
-        if ((actCond < conditions.Count && conditions[actCond] > actSprings) 
-            || actCond < conditions.Count - 1) {
-            return Conditions.Underflow;
+        HashSet<string> checkedAlready = new HashSet<string>();
+
+        private enum Conditions { Satisfied, Overflow, Underflow }
+        private Conditions CheckLine(string line, List<int> conditions) {
+            //Console.Write($"Checking:{line}, {string.Join(",", conditions)} ");
+            int actCond = 0;
+            int actSprings = 0;
+            int totalSprings = 0;
+            for (int i = 0; i < line.Length; i++) {
+                if (line[i] == '#') {
+                    actSprings++;
+                    totalSprings++;
+                }
+                else {
+                    if (actSprings > 0) {
+                        if (actCond < conditions.Count && actSprings < conditions[actCond]) return Conditions.Underflow;
+                        actSprings = 0;
+                        actCond++;
+                    }
+                }
+            }
+            if (totalSprings > conditions.Sum()) return Conditions.Overflow;
+            if ((actCond < conditions.Count && conditions[actCond] > actSprings)
+                || actCond < conditions.Count - 1) {
+                return Conditions.Underflow;
+            }
+            return Conditions.Satisfied;
         }
-        return Conditions.Satisfied;
     }
+
 }
