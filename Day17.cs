fa0a8689f3b0f326e06f0e979022f3a8a3dfedb2
Sarok Ádám <sarok.adam@gmail.com>
2024-11-09 22:39:42 +0100

    day17

--- Day17.cs
+++ Day17.cs
@@ -16,6 +16,20 @@ public class Day17
         //3. options are go straight, left, right
         //4. find minimal heat loss going from top left to bottom right corner
 
-        //this is a graph we can use Dijkstra on?
+        //this is a graph we can use Dijkstra
+        //first try without the 3 step rule?
+        //implement with priority queue
+    }
+
+    public class Dijkstra(List<string> input) {
+        int[,] distances;
+        // public int Solve() {
+        //     distances = new int[input[0].Length, input.Count];
+        //     for (int x = 0; x < input[0].Length) {
+        //         for (int y = 0; y < input.Count; y++) {
+        //             distances[x, y] = int.MaxValue;
+        //         }
+        //     }
+        // }
     }
 }
