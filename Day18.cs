0f1731396a640cf57135723b42c901012e8f763c
Ádám Sarok <sarok.adam@gmail.com>
2024-11-13 23:16:36 +0100

    day18

--- Day18.cs
+++ Day18.cs
@@ -7,13 +7,29 @@ namespace AdventOfCode;
 
 public class Day18 {
 	public static void SolvePart1() {
-        // var input = ReadFile("shortinput.txt");
-        // var s = new Solver(input, true);
-        // s.Solve();
-        var input2 = ReadFile("testinput.txt"); 
-        var s2 = new Solver(input2, true);
-        s2.Solve();
+        var input = ReadFile("shortinput.txt");
+        var s = new Solver(input, true);
+        s.Solve();
+        // var input2 = ReadFile("testinput.txt"); 
+        // var s2 = new Solver(input2, true);
+        // s2.Solve();
     }
+	public static void SolvePart2() {
+		// var input = ReadFilePart2("shortinput.txt");
+		// var s = new SolverShoeLace(input);
+		// s.Solve();
+		// var input2 = ReadFile("testinput.txt"); 
+		// var s2 = new Solver(input2, true);
+		// s2.Solve();
+	}
+
+	class SolverShoeLace(List<Input> inputs) {
+		//fill is too slow for this giant matrix - apply shoelace method
+		//1. convert the matrix into a clockwise collection of vertices in coordinate system
+		//current [y,x] is different than matrix [x,y]
+		//apply shoelace method to calc
+	}
+	
     public enum Directions { R, D, L, U }
     record struct Input(Directions dir, int length, string color) { }
 
@@ -33,6 +49,8 @@ public class Day18 {
         int height, width;
         private int xInside, yInside;
         private int circumference = 0;
+        private int minX = Int32.MaxValue, minY = Int32.MaxValue, maxX = 0, maxY = 0;
+        record struct Vertex(int x, int y) {}
         public void Solve() {
 	        Stopwatch sw = Stopwatch.StartNew();
             InitMatrix();
@@ -71,37 +89,25 @@ public class Day18 {
 	        }
 	        Console.WriteLine(cnt);
         }
-		record struct Vertex(int x, int y) {}
         private void Fill() {
-	        //1. find a pixel suspected to be inside
-	        //2. raycast, if inside start fill - fail
-	        
 	        //just a simple recursive fill - fill all outside than whats left is what we want
 	        //since we grew the matrix +1 in all direction, we can nicely flood fill the outside
 
 	        var q = new Queue<Vertex>();
+	        dig[0, 0] = 'X';
 	        q.Enqueue(new Vertex(0, 0));
 	        Vertex v;
 	        while (q.TryDequeue(out v)) {
-		        dig[v.y, v.x] = 'X';
 		        TryQueue(q, v.x, v.y + 1);
 		        TryQueue(q, v.x, v.y - 1);
 		        TryQueue(q, v.x + 1, v.y);
 		        TryQueue(q, v.x - 1, v.y);
 	        }
-	        
-	        // bool startRaycast = false;
-	        // for (int x = 0; x < width; x++) {
-	        //  for (int y = 0; y < height; y++) {
-	        //   if (dig[y, x] != '#' && IsInside(x, y)) {
-	        //    dig[y, x] = 'F';
-	        //   }
-	        //  }
-	        // }
         }
 
         private void TryQueue(Queue<Vertex> q, int x, int y) {
 	        if (x >= 0 && x < width && y >= 0 && y < height && dig[y, x] == '.') {
+		        dig[y, x] = 'X';
 		        q.Enqueue(new Vertex(x,y));
 	        }
         }
@@ -122,6 +128,7 @@ public class Day18 {
 	        int x = (int)width / 2;
 	        dig[y, x] = '#';
 	        foreach (var input in inputs) {
+		        circumference += input.length;
 		        switch (input.dir) {
 			        case Directions.U: Mark(ref x, ref y, dirX: 0, dirY: -1, length: input.length); break;
 			        case Directions.D: Mark(ref x, ref y, dirX: 0, dirY: 1, length: input.length); break;
@@ -130,51 +137,17 @@ public class Day18 {
 		        }
 	        }
         }
-
-		/*
-		private bool IsInside(int x, int y) {
-            //raycast - even=outside, odd=inside
-            //TODO: not correct this case fails to detect as inside:
-			// .....###
-			// ###..#F#
-            return RayCast(x, y, 0, 1)
-                    && RayCast(x, y, 0, -1)
-                    && RayCast(x, y, 1, 0)
-                    && RayCast(x, y, -1, 0);
-        }
-
-        private bool RayCast(int x, int y, int xDir, int yDir) {
-	        int intersects = 0;
-	        bool isIntersection = false;
-	        int actX = x;
-	        int actY = y;
-	        while (actX >= 0 && actX < width && actY >= 0 && actY <= height) {
-		        if (isIntersection && dig[actY, actX] != '#') {
-			        intersects++;
-			        isIntersection = false;
-		        }
-		        else if (dig[actY, actX] == '#') isIntersection = true;
-
-		        actX += xDir;
-		        actX += yDir;
-	        }
-	        return intersects % 2 == 1;
-        }
-		*/
+		
         private void PrintOut() { 
 	        using (var sw = new StreamWriter(new FileStream("output.txt", FileMode.OpenOrCreate))) {
 		        for (int y = 0; y < height; y++) {
 			        for (int x = 0; x < width; x++) {
 				        sw.Write(dig[y, x]);
-				        //Console.Write(dig[y, x]);
 			        }
 			        sw.WriteLine();
-			        //Console.WriteLine("");
 		        }
 	        }
         }
-
-        private int minX = Int32.MaxValue, minY = Int32.MaxValue, maxX = 0, maxY = 0;
         private void Mark(ref int x, ref int y, int dirX, int dirY, int length) {
             for (int i = 0; i < length; i++) {
                 y += dirY;
