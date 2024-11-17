f23b27cd827714a200bfe0b8a060c6bd3692bdb0
Ádám Sarok <sarok.adam@gmail.com>
2024-11-17 09:14:26 +0100

    day19

--- Day19.cs
+++ Day19.cs
@@ -14,14 +14,12 @@ public class Day19 {
 	public static void SolvePart2() {
 		var s = new SolverPart2("shortinput.txt");
 		s.Solve();
-		// var s2 = new Solver("testinput.txt");
-		// s2.Solve();
+		var s2 = new SolverPart2("testinput.txt");
+		s2.Solve();
 	}
 
 	public class SolverPart2(string inputFileName) : SolverBase(inputFileName) {
-		record struct Interval(Rule.TargetFields targetField, int from, int to) { }
-
-		//private List<Interval> intervals;
+		record struct Interval(Rule.TargetFields targetField, long from, long to) { }
 
 		//brute force would take 243 years so that is not very feasible
 		//build up ranges from all rules, so we have a list of intervals:
@@ -35,6 +33,7 @@ public class Day19 {
 			else {
 				toSplit = intervals.FirstOrDefault(x => x.from < value && x.to >= value);
 			}
+
 			if (toSplit != null) {
 				var val = toSplit.Value;
 				intervals.Remove(val);
@@ -47,21 +46,24 @@ public class Day19 {
 					n1 = new Interval(val.targetField, val.from, value - 1);
 					n2 = new Interval(val.targetField, value, val.to);
 				}
-				intervals.Add(new Interval(val.targetField, val.from, value));
-				intervals.Add(new Interval(val.targetField, value + 1, val.to));
-				Console.WriteLine($"Relation: {rel.ToString()}, Value: {value}");
-				Console.WriteLine($"Removed: {val}");
-				Console.WriteLine($"Split into: {n1}, {n2}");
+
+				if (n1.from > 0 && n1.from <= 4000 && n1.to > 0 && n1.to <= 4000) intervals.Add(n1);
+				if (n2.from > 0 && n2.from <= 4000 && n2.to > 0 && n2.to <= 4000) intervals.Add(n2);
+				// Console.WriteLine($"Relation: {rel.ToString()}, Value: {value}");
+				// Console.WriteLine($"Removed: {val}");
+				// Console.WriteLine($"Split into: {n1}, {n2}");
 			}
 		}
 
 		public void Solve() {
 			ReadInput();
-			Dictionary<Rule.TargetFields, List<Interval>> intervals = new Dictionary<Rule.TargetFields, List<Interval>>();
+			Dictionary<Rule.TargetFields, List<Interval>> intervals =
+				new Dictionary<Rule.TargetFields, List<Interval>>();
 			intervals.Add(Rule.TargetFields.a, [new Interval(Rule.TargetFields.a, 1, 4000)]);
-			intervals.Add(Rule.TargetFields.x, [new Interval(Rule.TargetFields.a, 1, 4000)]);
-			intervals.Add(Rule.TargetFields.m, [new Interval(Rule.TargetFields.a, 1, 4000)]);
-			intervals.Add(Rule.TargetFields.s, [new Interval(Rule.TargetFields.a, 1, 4000)]);
+			intervals.Add(Rule.TargetFields.x, [new Interval(Rule.TargetFields.x, 1, 4000)]);
+			intervals.Add(Rule.TargetFields.m, [new Interval(Rule.TargetFields.m, 1, 4000)]);
+			intervals.Add(Rule.TargetFields.s, [new Interval(Rule.TargetFields.s, 1, 4000)]);
+
 			//collect all points in a,x,m,s where a new interval is needed
 			//later run the same rule matching as day 1, but only for 1 number in all intervals
 			foreach (var wf in workflows) {
@@ -70,15 +72,13 @@ public class Day19 {
 					SplitIntervals(r.Rel, r.Value, ref list);
 				}
 			}
-			
-			Console.WriteLine("Result:");
+
 			foreach (var kvp in intervals) {
-				Console.WriteLine($"{kvp.Key}:");
-				foreach (var a in kvp.Value.OrderBy(x => x.from)) {
-					Console.WriteLine(a);
-				}
+				Console.WriteLine($"{kvp.Key}: {kvp.Value.Count} intervals");
+				// foreach (var a in kvp.Value.OrderBy(x => x.from)) {
+				// 	Console.WriteLine(a);
+				// }
 			}
-			//a1 -> m1,x1,s1, a1->m2,x1,s1 etc
 
 			foreach (var a in intervals[Rule.TargetFields.a]) {
 				foreach (var x in intervals[Rule.TargetFields.x]) {
@@ -86,19 +86,24 @@ public class Day19 {
 						foreach (var s in intervals[Rule.TargetFields.s]) {
 							var dummy = new Part(x.from, m.from, a.from, s.from);
 							var rDummy = TraverseWorkflows(dummy, "in");
-							var dummy2 = new Part(x.to - 1, m.to - 1, a.to - 1, s.to - 1);
+							var dummy2 = new Part(x.to, m.to, a.to, s.to);
 							var rDummy2 = TraverseWorkflows(dummy2, "in");
 							if (rDummy != rDummy2) {
 								throw new Exception(
 									$"{dummy}={rDummy} does not equal {dummy2}={rDummy2}"); //if the ranges are set correctly, both start & end of range should be the same result either A or R
 							}
-							//TODO: result is still not OK, even though the cross check abover works
+
+							//TODO: shortinput works, testinput fails:
+							//Part { x = 881, m = 1609, a = 1, s = 2010 }= A does not equal Part { x = 902, m = 1610, a = 98, s = 2039 }= R
+							//Part { x = 1251, m = 1609, a = 1, s = 2010 }=A does not equal Part { x = 1269, m = 1610, a = 98, s = 2039 }=R
 							if (rDummy == Results.A)
-								result += (a.to - a.from) * (m.to - m.from) * (x.to - x.from) * (s.to - s.from);
+								result += (a.to - a.from + 1) * (m.to - m.from + 1) * (x.to - x.from + 1) *
+								          (s.to - s.from + 1);
 						}
 					}
 				}
 			}
+
 			Console.WriteLine($"Result: {result}");
 		}
 	}
@@ -168,6 +173,11 @@ public class Day19 {
 						rules.Add(Rule.ParseRule(rule));
 					}
 
+					//there are tons of dummy rules eg.: ld{x<1281:R,R} this is always a reject and can be replaced with ld{R}
+					string next = rules[0].NextStep;
+					if (rules.All(x => x.NextStep == next))
+						rules = [new Rule(Rule.Relations.Fallback, Rule.TargetFields.a, 1, next)];
+
 					workflows.Add(w[0], rules);
 				}
 				else {
@@ -181,7 +191,7 @@ public class Day19 {
 			}
 		}
 
-		protected record struct Part(int x, int m, int a, int s);
+		protected record struct Part(long x, long m, long a, long s);
 
 		protected class Rule(Rule.Relations rel, Rule.TargetFields targetField, int value, string nextStep) {
 			public string NextStep => nextStep;
