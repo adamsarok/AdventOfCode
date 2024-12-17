adc6ee1f614e299e5d82172160d873bf924a88bc
Sarok Ádám <sarok.adam@gmail.com>
2024-12-17 16:07:20 +0100

    feat: 2024 day 16 still incomplete, we only find & mark one optimal route

--- Year2024/Day16/Day16.cs
+++ Year2024/Day16/Day16.cs
@@ -27,6 +27,7 @@ namespace Year2024.Day16 {
 
 		protected override long SolvePart1() {
 			result = long.MaxValue;
+			optimalPaths = new bool[input.Length, input[0].Length];
 			iter = 0;
 			sw.Restart();
 			costs = new long[input.Length, input[0].Length];
@@ -71,21 +72,23 @@ namespace Year2024.Day16 {
 			}
 		}
 		long[,] costs;
-		private void GoFrom(Point point, Direction facing, long currentScore) {
+		bool[,] optimalPaths;
+		private bool GoFrom(Point point, Direction facing, long currentScore) {
 			iter++;
-			Go(point, facing, facing, currentScore); //first try straight line
+			if (Go(point, facing, facing, currentScore)) return true; //first try straight line
 			switch (facing) {
 				case Direction.Up:
 				case Direction.Down:
-					Go(point, facing, Direction.Left, currentScore);
-					Go(point, facing, Direction.Right, currentScore);
+					if (Go(point, facing, Direction.Left, currentScore)) return true;
+					if (Go(point, facing, Direction.Right, currentScore)) return true;
 					break;
 				case Direction.Left:
 				case Direction.Right:
-					Go(point, facing, Direction.Up, currentScore);
-					Go(point, facing, Direction.Down, currentScore);
+					if (Go(point, facing, Direction.Up, currentScore)) return true;
+					if (Go(point, facing, Direction.Down, currentScore)) return true;
 					break;
 			}
+			return false;
 		}
 		private int GetTurns(Direction currentFacing, Direction dirToGo) {
 			switch (currentFacing) {
@@ -140,7 +143,7 @@ namespace Year2024.Day16 {
 			}
 			throw new Exception("shouldn't happen");
 		}
-		private void Go(Point from, Direction currentFacing, Direction dirToGo, long currentScore) {
+		private bool Go(Point from, Direction currentFacing, Direction dirToGo, long currentScore) {
 			//Debug();
 			Point dest;
 			switch (dirToGo) {
@@ -159,15 +162,22 @@ namespace Year2024.Day16 {
 				default:
 					throw new Exception("shouldn't happen");
 			}
-			if (dest.y < 0 || dest.y >= input.Length || dest.x < 0 || dest.x >= input.Length) return;
-			if (input[dest.y][dest.x] == '#') return;
+			if (dest.y < 0 || dest.y >= input.Length || dest.x < 0 || dest.x >= input.Length) return false;
+			if (input[dest.y][dest.x] == '#') return false;
 			long nextScore = currentScore + (GetTurns(currentFacing, dirToGo) * 1000) + 1;
-			if (costs[dest.y, dest.x] < nextScore) return;
+			if (costs[dest.y, dest.x] < nextScore) return false;
 			costs[dest.y, dest.x] = nextScore;
 			if (input[dest.y][dest.x] == 'E') {
 				result = Math.Min(nextScore, result);
+				if (nextScore == target) {
+					return true;
+				}
 			}
-			GoFrom(dest, dirToGo, nextScore);
+			var r = GoFrom(dest, dirToGo, nextScore);
+			if (r) {
+				optimalPaths[dest.y, dest.x] = true;
+			}
+			return r;
 		}
 		private void PrintCosts() {
 			Console.Clear();
@@ -180,24 +190,35 @@ namespace Year2024.Day16 {
 				Console.WriteLine();
 			}
 		}
+		private void PrintOptimalPaths() {
+			Console.Clear();
+			for (int y = 0; y < input.Length; y++) {
+				for (int x = 0; x < input.Length; x++) {
+					if (optimalPaths[y, x]) {
+						Console.Write('X');
+					} else Console.Write(input[y][x]);
+				}
+				Console.WriteLine();
+			}
+		}
 
-//We can find one best path with this logic, but not all best paths
-//3008 4009 3010 -> here the path through 4009 is skipped even though its the same cost as through 3009
-//#    #    #    #    #    #    #    #    #    #    #    #    #    #    #
-//#  5016 6017 6018 6019 6020 6021 6022   #  8040 8039 8038 8037 7036   #
-//#  5015   #  7017   #    #    #  7023   #  9023   #    #    #  7035   #
-//#  5014 6015 6016 6017 6018   #  7024   #  8022 8021 7020   #  7034   #
-//#  5013   #    #    #  7019   #    #    #    #    #  7019   #  7033   #
-//#  5012   #  3010   #  6020 6019 6018 6017 5016 6017 6018   #  7032   #
-//#  5011   #  3009   #    #    #    #    #  5015   #    #    #  7031   #
-//#  4010 4009 3008 4009 3010 4011 4012 4013 4014 4015 4016   #  7030   #
-//#    #    #  3007   #  3009   #    #    #    #    #  5017   #  7029   #
-//#  1004 2005 2006   #  3008 4009 4010 4011 4012   #  5018   #  7028   #
-//#  1003   #  3005   #  3007   #    #    #  5013   #  5019   #  7027   #
-//#  1002 2003 2004 2005 2006   #  5012 6013 5014   #  5020   #  7026   #
-//#  1001   #    #    #  3007   #  5011   #  5013   #  5021   #  7025   #
-//#   -1    1    2    #  3008 4009 4010 4011 4012   #  5022 6023 6024   #
-//#    #    #    #    #    #    #    #    #    #    #    #    #    #    #
+		//We can find one best path with this logic, but not all best paths
+		//3008 4009 3010 -> here the path through 4009 is skipped even though its the same cost as through 3009
+		//#    #    #    #    #    #    #    #    #    #    #    #    #    #    #
+		//#  5016 6017 6018 6019 6020 6021 6022   #  8040 8039 8038 8037 7036   #
+		//#  5015   #  7017   #    #    #  7023   #  9023   #    #    #  7035   #
+		//#  5014 6015 6016 6017 6018   #  7024   #  8022 8021 7020   #  7034   #
+		//#  5013   #    #    #  7019   #    #    #    #    #  7019   #  7033   #
+		//#  5012   #  3010   #  6020 6019 6018 6017 5016 6017 6018   #  7032   #
+		//#  5011   #  3009   #    #    #    #    #  5015   #    #    #  7031   #
+		//#  4010 4009 3008 4009 3010 4011 4012 4013 4014 4015 4016   #  7030   #
+		//#    #    #  3007   #  3009   #    #    #    #    #  5017   #  7029   #
+		//#  1004 2005 2006   #  3008 4009 4010 4011 4012   #  5018   #  7028   #
+		//#  1003   #  3005   #  3007   #    #    #  5013   #  5019   #  7027   #
+		//#  1002 2003 2004 2005 2006   #  5012 6013 5014   #  5020   #  7026   #
+		//#  1001   #    #    #  3007   #  5011   #  5013   #  5021   #  7025   #
+		//#   -1    1    2    #  3008 4009 4010 4011 4012   #  5022 6023 6024   #
+		//#    #    #    #    #    #    #    #    #    #    #    #    #    #    #
 
 
 		long part2result;
@@ -206,7 +227,9 @@ namespace Year2024.Day16 {
 			part2result = 0;
 			SolvePart1();
 			target = result; //hacky
+			optimalPaths = new bool[input.Length, input[0].Length];
 			SolvePart1();
+			PrintOptimalPaths();
 			//PrintCosts();
 			//for (int y = 0; y < input.Length; y++) {
 			//	for (int x = 0; x < input[0].Length; x++) {
