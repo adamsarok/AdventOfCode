f61f085b70536b20006f55850614abab3210ae2e
Ádám Sarok <sarok.adam@gmail.com>
2024-12-18 19:08:17 +0100

    feat: 2024 day 18

--- Helpers/Point.cs
+++ Helpers/Point.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Helpers {
-	public record Point(int x, int y) {
+	public record Point(int x, int y) : IComparable<Point> {
 		public static Point operator +(Point a, Point b) =>
 			new Point(a.x + b.x, a.y + b.y);
 
@@ -17,5 +17,11 @@ namespace Helpers {
 
 		public static bool operator <=(Point a, Point b) =>
 			a.x <= b.x && a.y <= b.y;
+
+		public int CompareTo(Point? other) {
+			if (other == null) return 1;
+			if (x == other.x && y == other.y) return 0;
+			return (x * x + y * y).CompareTo(other.x * other.x + other.y * other.y);
+		}
 	}
 }
--- Year2024/Day18/Day18.cs
+++ Year2024/Day18/Day18.cs
@@ -4,35 +4,137 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Year2024.Day18 {
 	public class Day18 : Solver {
 		public Day18() : base(2024, 18) {
 		}
+		private bool[,] corruptedMemory; //x, y !!!
+		private int size;
+		string[] file;
+		private int byteLimit;
 		protected override void ReadInputPart1(string fileName) {
-			//input = new();
-			foreach (var l in File.ReadAllLines(fileName)) {
-
+			if (fileName.Contains("short")) {
+				size = 7;
+				byteLimit = 12;
+			} else {
+				size = 71;
+				byteLimit = 1024;
 			}
+			corruptedMemory = new bool[size, size];
+			int b = 0;
+			file = File.ReadAllLines(fileName);
+			foreach (var l in File.ReadAllLines(fileName)) {
+				AddByte(l);
+				b++;
+				if (b >= byteLimit) break;
+			}
+		}
+
+		private void AddByte(string line) {
+			var s = line.Split(",");
+			corruptedMemory[int.Parse(s[0]), int.Parse(s[1])] = true;
 		}
 
 		protected override void ReadInputPart2(string fileName) {
-			//input = new();
-			foreach (var l in File.ReadAllLines(fileName)) {
-
-			}
+			ReadInputPart1(fileName);
 		}
 
-		protected override long SolvePart1() {
+		PriorityQueue<int, Point> queue;
+		//works on short input but way too long queue on final input
+		protected long SolvePriorityQueue() {
 			long result = 0;
+			costs = new long[size, size];
+			for (int x = 0; x < size; x++) {
+				for (int y = 0; y < size; y++) {
+					costs[x, y] = long.MaxValue;
+				}
+			}
+			costs[0, 0] = 0;
+			queue = new PriorityQueue<int, Point>();
+			queue.Enqueue(0, new Point(0, 0));
+			Point next;
+			int prio;
+			while (queue.TryDequeue(out prio, out next)) {
+				TryQueue(new Point(next.x - 1, next.y), prio + 1);
+				TryQueue(new Point(next.x + 1, next.y), prio + 1);
+				TryQueue(new Point(next.x, next.y - 1), prio + 1);
+				TryQueue(new Point(next.x, next.y + 1), prio + 1);
+			}
+			return costs[size-1, size-1];
+		}
 
-			return result;
+		private void TryQueue(Point point, int prio) {
+			if (point.x < 0 || point.y < 0 || point.x >= size || point.y >= size) return;
+			if (corruptedMemory[point.x, point.y]) return;
+			if (costs[point.x, point.y] < prio) return;
+			costs[point.x, point.y] = prio;
+			queue.Enqueue(prio, point);
+			//Debug();
+		}
+
+		private long[,] costs;
+		bool[,] dirty;
+		override protected long SolvePart1() {
+			dirty = new bool[size, size];
+			costs = new long[size, size];
+			for (int x = 0; x < size; x++) {
+				for (int y = 0; y < size; y++) {
+					costs[x, y] = long.MaxValue;
+				}
+			}
+			costs[0, 0] = 0;
+			dirty[0, 0] = true;
+			while (true) {
+				bool wasDirty = false;
+				for (int x = 0; x < size; x++) {
+					for (int y = 0; y < size; y++) {
+						if (dirty[x, y]) {
+							dirty[x, y] = false;
+							var cost = costs[x, y] + 1;
+							Process(x - 1, y, cost);
+							Process(x + 1, y, cost);
+							Process(x, y - 1, cost);
+							Process(x, y + 1, cost);
+							wasDirty = true;
+						}
+					}
+				}
+				if (!wasDirty) return costs[size - 1, size - 1];
+			}
+		}
+		private void Process(int x, int y, long cost) {
+			if (x < 0 || y < 0 || x >= size || y >= size) return;
+			if (corruptedMemory[x, y]) return;
+			if (costs[x, y] < cost) return;
+			costs[x, y] = cost;
+			dirty[x, y] = true;
+		}
+
+		private void Debug() {
+			Console.Clear();
+			for (int y = 0; y < size; y++) {
+				for (int x = 0; x < size; x++) {
+					if (corruptedMemory[x, y]) Console.Write("  X  ");
+					else Console.Write(costs[x, y] == long.MaxValue ? " 000 " : $" {costs[x, y].ToString("000")} ");
+				}
+				Console.WriteLine();
+			}
 		}
 
 		protected override long SolvePart2() {
 			long result = 0;
-
-			return result;
+			for (int i = byteLimit; i < file.Length; i++) {
+				var line = file[i];
+				AddByte(line);
+				var path = SolvePart1();
+				if (path == long.MaxValue) {
+					Console.WriteLine(line);
+					return 1;
+				}
+			}
+			throw new Oopsie("Didn't find blocking byte");
 		}
 	}
 }
