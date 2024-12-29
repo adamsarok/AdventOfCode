18a7ddcdf09cd802df955cb57e7596dac989a631
Ádám Sarok <sarok.adam@gmail.com>
2024-12-29 02:06:46 +0100

    feat: 2019 day 09

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,6 +1,4 @@
-﻿using Year2019.Day07;
-
-var s1 = new Year2019.Day07.Day07();
+﻿var s1 = new Year2019.Day09.Day09();
 s1.Solve();
 
 //var sb = new Year2021.Day03.Day03();
--- Year2019/Day09/Day09.cs
+++ Year2019/Day09/Day09.cs
@@ -7,32 +7,156 @@ using System.Threading.Tasks;
 
 namespace Year2019.Day09 {
 	public class Day09 : Solver {
+		private long[] startCode;
+
 		public Day09() : base(2019, 9) {
 		}
 		protected override void ReadInputPart1(string fileName) {
-			//input = new();
-			foreach (var l in File.ReadAllLines(fileName)) {
-
+			base.ReadInputPart1(fileName);
+			IsShort = fileName.Contains("short");
+			var cmds = File.ReadAllLines(fileName)[0].Split(',');
+			startCode = new long[cmds.Length];
+			for (int i = 0; i < cmds.Length; i++) {
+				startCode[i] = long.Parse(cmds[i]);
 			}
 		}
 
 		protected override void ReadInputPart2(string fileName) {
-			//input = new();
-			foreach (var l in File.ReadAllLines(fileName)) {
+			base.ReadInputPart2(fileName);
+			ReadInputPart1(fileName);
+		}
 
+		class IntCodeComputer {
+			long[] intCode;
+			long relativeBase = 0;
+			List<long> output;
+			long instructionPointer;
+			public IntCodeComputer(long[] startCode) {
+				intCode = new long[10000];
+				instructionPointer = 0;
+				output = new List<long>();
+				for (int i = 0; i < startCode.Length; i++) intCode[i] = startCode[i];
+			}
+
+			long GetVariable(long pos, ParamMode mode) {
+				switch (mode) {
+					case ParamMode.Position:
+						return intCode[intCode[pos]];
+					case ParamMode.Immediate:
+						return intCode[pos];
+					case ParamMode.Relative:
+						return intCode[intCode[pos] + relativeBase];
+					default: throw new Oopsie("unknown ParamMode");
+				}
+			}
+			void SetVariable(long pos, long val, ParamMode mode) {
+				switch (mode) {
+					case ParamMode.Position:
+						intCode[intCode[pos]] = val;
+						break;
+					case ParamMode.Immediate:
+						intCode[intCode[pos]] = val;
+						break;
+					case ParamMode.Relative:
+						intCode[intCode[pos] + relativeBase] = val;
+						break;
+					default: throw new Oopsie("unknown ParamMode");
+				}
+			}
+
+			enum Opcode {
+				ADD = 1,
+				MUL = 2,
+				IN = 3,
+				OUT = 4,
+				JIT = 5,
+				JIF = 6,
+				LT = 7,
+				EQ = 8,
+				RB = 9,
+				HALT = 99
+			}
+			enum ParamMode {
+				Position = 0,
+				Immediate = 1,
+				Relative = 2
+			}
+			record Command(Opcode opcode, ParamMode aMode, ParamMode bMode, ParamMode cMode);
+			Command ParseCommand(long cmdNum) {
+				var opcode = (Opcode)(cmdNum % 100);
+				if (opcode == 0) throw new Oopsie();
+				long modes = cmdNum / 100;
+				var aMode = (ParamMode)(modes % 10);
+				modes /= 10;
+				var bMode = (ParamMode)(modes % 10);
+				modes /= 10;
+				var cMode = (ParamMode)(modes % 10);
+				return new Command(opcode, aMode, bMode, cMode);
+			}
+
+			long param1(Command cmd) => GetVariable(instructionPointer + 1, cmd.aMode);
+			long param2(Command cmd) => GetVariable(instructionPointer + 2, cmd.bMode);
+			public long RunCode(long input) {
+				while (instructionPointer < intCode.Length) {
+					var cmd = ParseCommand(intCode[instructionPointer]);
+					switch (cmd.opcode) {
+						case Opcode.ADD:
+							SetVariable(instructionPointer + 3, param1(cmd) + param2(cmd), cmd.cMode);
+							instructionPointer += 4;
+							break;
+						case Opcode.MUL:
+							SetVariable(instructionPointer + 3,
+								param1(cmd) * param2(cmd), cmd.cMode);
+							instructionPointer += 4;
+							break;
+						case Opcode.IN:
+							SetVariable(instructionPointer + 1, input, cmd.aMode);
+							instructionPointer += 2;
+							break;
+						case Opcode.OUT:
+							Output(param1(cmd));
+							instructionPointer += 2;
+							break;
+						case Opcode.JIT:
+							if (param1(cmd) != 0) instructionPointer = param2(cmd);
+							else instructionPointer += 3;
+							break;
+						case Opcode.JIF:
+							if (param1(cmd) == 0) instructionPointer = param2(cmd);
+							else instructionPointer += 3;
+							break;
+						case Opcode.LT:
+							SetVariable(instructionPointer + 3, param1(cmd) < param2(cmd) ? 1 : 0, cmd.cMode);
+							instructionPointer += 4;
+							break;
+						case Opcode.EQ:
+							SetVariable(instructionPointer + 3, param1(cmd) == param2(cmd) ? 1 : 0, cmd.cMode);
+							instructionPointer += 4;
+							break;
+						case Opcode.RB:
+							relativeBase += param1(cmd);
+							instructionPointer += 2;
+							break;
+						case Opcode.HALT:
+							return output.Last();
+					}
+				}
+				return -1;
+			}
+
+			private void Output(long v) {
+				output.Add(v);
 			}
 		}
 
 		protected override long SolvePart1() {
-			long result = 0;
-
-			return result;
+			var compy = new IntCodeComputer(startCode);
+			return compy.RunCode(1);
 		}
 
 		protected override long SolvePart2() {
-			long result = 0;
-
-			return result;
+			var compy = new IntCodeComputer(startCode);
+			return compy.RunCode(2);
 		}
 	}
 }
