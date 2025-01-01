6e68145420b82e041762972b11b05cef5f731ef8
Ádám Sarok <sarok.adam@gmail.com>
2025-01-01 01:54:00 +0100

    feat: 2019 day 25

--- Helpers/Extensions.cs
+++ Helpers/Extensions.cs
@@ -54,5 +54,18 @@ namespace Helpers {
 				}
 			}
 		}
+
+		public static IEnumerable<T[]> SetCombinations<T>(this IEnumerable<T> source) {
+			if (null == source)
+				throw new ArgumentNullException(nameof(source));
+
+			T[] data = source.ToArray();
+
+			return Enumerable
+			  .Range(0, 1 << (data.Length))
+			  .Select(index => data
+				 .Where((v, i) => (index & (1 << i)) != 0)
+				 .ToArray());
+		}
 	}
 }
--- Year2019/Day25/Day25.cs
+++ Year2019/Day25/Day25.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static Helpers.Directions;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Year2019.Day25 {
 	public class Day25 : Solver {
@@ -117,7 +118,7 @@ namespace Year2019.Day25 {
 							var c = param1(aMode);
 							output.Add((char)c);
 							instructionPointer += 2;
-							if (c == '?') return new string(output.ToArray());
+							if (CheckOutputFinished()) return new string(output.ToArray());
 							break;
 						case Opcode.JIT:
 							if (param1(aMode) != 0) instructionPointer = param2(bMode);
@@ -145,9 +146,18 @@ namespace Year2019.Day25 {
 				}
 				return "-1"; //shouldn't happen
 			}
+
+			private bool CheckOutputFinished() { //there must be a better way than this
+				var l = output.Count;
+				//Command?
+				if (output[l - 1] == '?' && output[l - 2] == 'd' && output[l-8] == 'C') return true;
+				return false;
+			}
+
 		}
 
-		private class Room {
+
+		private class Room : IEquatable<Room> {
 			public LVec Pos { get; set; }
 			public string InitString { get; }
 			public string Name { get; }
@@ -182,6 +192,11 @@ namespace Year2019.Day25 {
 					}
 				}
 			}
+
+			bool IEquatable<Room>.Equals(Room? other) {
+				if (other != null && other.Name == Name) return true;
+				return false;
+			}
 		}
 			
 		private List<Room> rooms;
@@ -207,36 +222,89 @@ namespace Year2019.Day25 {
 			return null;
 		}
 
-		List<string> dontTake = new List<string>() { "photons" };
+		List<string> dontTake = new List<string>() { "photons", "infinite loop", "giant electromagnet", "escape pod", "molten lava" };
+		List<string> inventory = new List<string>();
+		Room sensorRoom;
 
 		private long Traverse(bool isFirstPart) {
-			Console.Clear();
+			//Console.Clear();
 			var compy = new IntCodeComputer(startCode);
 			rooms = new List<Room>();
 
 			string output = compy.RunCode("\n", isFirstPart);
 			var room = new Room(output);
 			rooms.Add(room);
-			Console.WriteLine(output);
+			//Console.WriteLine(output);
 			while (true) {
 				var next = FindFirstUnvisited(room);
-				if (next == null) return -1; //all rooms visited, no solution
-				var dir = next.Neighbors.FirstOrDefault(x => x.Value == null).Key;
+				if (next == null) {
+					//Console.Clear();
+					var r = compy.RunCode($"inv\n", isFirstPart);
+					//Console.WriteLine(r);
+					return DoWeightPuzzle(compy, room, isFirstPart);
+				}
 				if (next.Name == room.Name) {
-					room = Navigate(compy, dir, isFirstPart, room);
-					foreach (var item in room.Items.Except(dontTake)) {
-						//Console.WriteLine($"Taking {item}");
-						Console.WriteLine(compy.RunCode($"take {item}\n", isFirstPart));
-						//Console.WriteLine(compy.RunCode($"inv\n", isFirstPart));
-					}
+					var dir = next.Neighbors.FirstOrDefault(x => x.Value == null).Key;
+					room = NavigateToDirection(compy, dir, isFirstPart, room);
 				} else {
-					return -1; //todo: navigate to unvisited room
+					room = NavigateToRoom(isFirstPart, compy, room, next);
 				}
 			}
 			return -1;
 		}
 
-		private Room Navigate(IntCodeComputer compy, Direction direction, bool isFirstPart, Room from) {
+		private Room NavigateToRoom(bool isFirstPart, IntCodeComputer compy, Room from, Room to) {
+			var room = from;
+			var path = MapPath(from, to, new List<Room>());
+			if (path.Count == 0) throw new Oopsie($"No path found from {from.Name} to {to.Name}");
+			foreach (var p in path) {
+				var d = room.Neighbors.Where(x => x.Value.Equals(p)).First().Key;
+				room = NavigateToDirection(compy, d, isFirstPart, room);
+			}
+			//Console.WriteLine($"Success navigating {from.Name} to {to.Name}");
+			return room;
+		}
+
+		
+
+		private long DoWeightPuzzle(IntCodeComputer compy, Room from, bool isFirstPart) {
+			var precheck = rooms.Where(x => x.Name == "== Security Checkpoint ==").First();
+			var room = NavigateToRoom(isFirstPart, compy, from, precheck);
+			var dir = room.Neighbors.Where(x => x.Value.Name == "== Pressure-Sensitive Floor ==").First().Key;
+			bool success = false;
+			string dirstr = GetDirStr(dir);
+			var combinations = inventory.SetCombinations();
+			foreach (var c in combinations) {
+				foreach (var all in inventory) compy.RunCode($"take {all}\n", isFirstPart);
+				foreach (var drop in inventory.Except(c)) compy.RunCode($"drop {drop}\n", isFirstPart);
+
+				//Console.Clear();
+				//Console.WriteLine(compy.RunCode($"inv\n", isFirstPart));
+				var r = compy.RunCode($"{dirstr}\n", isFirstPart);
+				//Console.WriteLine(r);
+				if (!r.Contains("lighter") && !r.Contains("heavier")) {
+					var key = "get in by typing ";
+					var result = r.Substring(r.IndexOf(key) + key.Length, 10);
+					return long.Parse(result);
+				}
+			}
+			return -1;
+		}
+
+		List<Room> MapPath(Room from, Room to, List<Room> visited) {
+			foreach (var neighbor in from.Neighbors.Values.Where(x => x != null)) {
+				if (visited.Any(x => x.Equals(neighbor))) {
+					continue;
+				}
+				var pathToNeighbor = new List<Room>(visited);
+				pathToNeighbor.Add(neighbor);
+				if (neighbor == to) return pathToNeighbor;
+				var n = MapPath(neighbor, to, pathToNeighbor);
+				if (n.Count > 0) return n;
+			}
+			return new List<Room>();
+		}
+		private string GetDirStr(Direction direction) {
 			string dirstr = direction switch {
 				Direction.North => "north",
 				Direction.South => "south",
@@ -244,11 +312,17 @@ namespace Year2019.Day25 {
 				Direction.East => "east",
 				_ => throw new Oopsie("unknown direction")
 			};
-			Console.ForegroundColor = ConsoleColor.DarkMagenta;
-			Console.WriteLine($"{dirstr}");
+			return dirstr;
+		}
+		private Room NavigateToDirection(IntCodeComputer compy, Direction direction, bool isFirstPart, Room from) {
+			//Console.Clear();
+			string dirstr = GetDirStr(direction);
+			//Console.ForegroundColor = ConsoleColor.DarkMagenta;
+			//Console.WriteLine($"{dirstr}");
 			var r = compy.RunCode($"{dirstr}\n", isFirstPart);
 			if (!r.Contains("can't")) {
-				Console.WriteLine($"Success!");
+				if (sensorRoom == null && r.Contains("Pressure-Sensitive Floor")) return WeightPuzzleFound(compy, from, isFirstPart, r, direction);
+				//Console.WriteLine($"Success!");
 				Room room;
 				if (!from.Neighbors.TryGetValue(direction, out room) || room == null) {
 					room = new Room(r);
@@ -256,20 +330,39 @@ namespace Year2019.Day25 {
 					room.Neighbors[Opposite(direction)] = from;
 					rooms.Add(room);
 				}
-				Console.ResetColor();
-				Console.WriteLine(r);
+
+				foreach (var item in room.Items.Except(dontTake).Except(inventory)) {
+					//Console.ForegroundColor = ConsoleColor.Red;
+					//Console.WriteLine($"Taking: {item}");
+					//Console.ResetColor();
+					var r2 = compy.RunCode($"take {item}\n", isFirstPart);
+					//Console.WriteLine(r2);
+					inventory.Add(item);
+				}
+
+				//Console.ResetColor();
+				//Console.WriteLine(r);
 				return room;
+			} else {
+				throw new Oopsie($"can't navigate {direction} from {from.Name}");
 			}
-			Console.ResetColor();
-			Console.WriteLine(r);
-			return from;
+			//Console.ResetColor();
+			//Console.WriteLine(r);
+			//return from;
+		}
+
+		private Room WeightPuzzleFound(IntCodeComputer compy, Room from, bool isFirstPart, string initStr, Direction direction) {
+			if (from.Neighbors[direction] == null) {
+				var room = new Room(initStr.Substring(0, initStr.IndexOf("== Security Checkpoint ==")));
+				from.Neighbors[direction] = room;
+				sensorRoom = room;
+				if (initStr.Contains("ejected")) return from; //thrown back where we came from
+			}
+			throw new Oopsie("We expected to be ejected from the pressure sensor room but we did not :(");
 		}
 
 		protected override long SolvePart2() {
-			base.SolvePart2();
-			if (IsShort) return -1;
-			//SetAllCommands();
-			return Traverse(false);
+			return -1;
 		}
 	}
 }
