463f5999292540d0520b23f372a90a3b9f4dc1e3
Ádám Sarok <sarok.adam@gmail.com>
2025-01-02 09:51:27 +0100

    feat: 2019 day 10

--- Year2019/Day10/Day10.cs
+++ Year2019/Day10/Day10.cs
@@ -1,5 +1,6 @@
 using Helpers;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,7 +29,12 @@ namespace Year2019.Day10 {
 		}
 
 		protected override long SolvePart1() {
-			long result = 0;
+			var result = GetBestMonitoringStation();
+			return result.AsteroidsSeen;
+		}
+		record Station(long AsteroidsSeen, LVec Pos);
+		private Station GetBestMonitoringStation() {
+			long seen = 0; LVec pos = new LVec(0,0);
 			foreach (var start in asteroids) {
 				Dictionary<double, List<LVec>> asteroidsByAngleToMonitoringPoint = new Dictionary<double, List<LVec>>();
 				//1. get the angle from the monitoring point to all other asteroids
@@ -46,15 +52,54 @@ namespace Year2019.Day10 {
 					}
 					group.Add(other);
 				}
-				result = Math.Max(result, asteroidsByAngleToMonitoringPoint.Count);
+				if (asteroidsByAngleToMonitoringPoint.Count > seen) {
+					seen = asteroidsByAngleToMonitoringPoint.Count;
+					pos = start;
+				}
 			}
-			return result;
+			return new Station(seen, pos);
 		}
 
+		record Asteroid2(LVec Pos, long DistanceFromStart);
 		protected override long SolvePart2() {
-			long result = 0;
+			var start = GetBestMonitoringStation();
+			Dictionary<double, List<Asteroid2>> asteroidsByAngleToMonitoringPoint = new();
+			foreach (var other in asteroids.Where(x => x != start.Pos)) {
+				double deltaY = other.y - start.Pos.y;
+				double deltaX = other.x - start.Pos.x;
+				double angleInRadians = Math.Atan2(deltaY, deltaX);
+				double angleInDegrees = - angleInRadians * (180.0 / Math.PI);
+				List<Asteroid2> group;
+				if (!asteroidsByAngleToMonitoringPoint.TryGetValue(angleInDegrees, out group)) {
+					group = new();
+					asteroidsByAngleToMonitoringPoint.Add(angleInDegrees, group);
+				}
+				group.Add(new Asteroid2(other, other.ManhattanDistance(start.Pos)));
+			}
+			//start from 90° go clockwise, go in quadrants
+			int cnt = 0; LVec result = new LVec(0,0);
+			while (cnt < 200) {
+				var q = asteroidsByAngleToMonitoringPoint.Where(x => x.Key >= 0 && x.Key <= 90).OrderByDescending(x => x.Key);
+				RemoveFirsts(q, ref cnt, ref result);
+				q = asteroidsByAngleToMonitoringPoint.Where(x => x.Key >= -90 && x.Key <= 0).OrderByDescending(x => x.Key);
+				RemoveFirsts(q, ref cnt, ref result);
+				q = asteroidsByAngleToMonitoringPoint.Where(x => x.Key >= -180 && x.Key < -90).OrderByDescending(x => x.Key);
+				RemoveFirsts(q, ref cnt, ref result);
+				q = asteroidsByAngleToMonitoringPoint.Where(x => x.Key >= 90 && x.Key <= 180).OrderByDescending(x => x.Key);
+				RemoveFirsts(q, ref cnt, ref result);
+			}
+			return result.x * 100 + result.y;
+		}
 
-			return result;
+		private static void RemoveFirsts(IOrderedEnumerable<KeyValuePair<double, List<Asteroid2>>> q, ref int cnt, ref LVec result) {
+			foreach (var group in q) {
+				var asteroid = group.Value.OrderBy(x => x.DistanceFromStart).FirstOrDefault();
+				if (asteroid == null) continue;
+				cnt++;
+				group.Value.Remove(asteroid);
+				Console.WriteLine($"The {cnt}. asteroid to be vaporized is at {asteroid.Pos.x},{asteroid.Pos.y}");
+				if (cnt == 200) result = asteroid.Pos;
+			}
 		}
 	}
 }
