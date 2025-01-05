062ec7396597a700cd72de57e20b991d5623a78a
Ádám Sarok <sarok.adam@gmail.com>
2025-01-05 17:33:19 +0100

    feat: 2019 day 18 still too slow

--- Year2019/Day18/Day18.cs
+++ Year2019/Day18/Day18.cs
@@ -9,6 +9,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Net.Mime.MediaTypeNames;
+using static Year2019.Day18.Day18.Dijkstra18;
 
 namespace Year2019.Day18 {
 	//slow and will not finish on the full dataset. on short examples can be used to cross check
@@ -46,12 +47,9 @@ namespace Year2019.Day18 {
 		record KeyDistance(char key1, long key1bits, char key2, long key2bits, long neededKeys, long distance);
 		Dictionary<char, List<KeyDistance>> keyDistances;
 
-		protected override void ReadInputPart2(string fileName) {
-			ReadInputPart1(fileName);
-		}
-
 		Stopwatch sw;
 		protected override long SolvePart1() {
+			return -1;
 			keyDistances = new();
 			minimumCosts = new();
 			sw = Stopwatch.StartNew();
@@ -61,7 +59,7 @@ namespace Year2019.Day18 {
 				keyDistances.Add(key.Key, new List<KeyDistance>());
 				foreach (var other in keys.Where(x => x.Key != key.Key)) {
 					var cost = costs[other.Value.x, other.Value.y];
-					keyDistances[key.Key].Add(new KeyDistance(key.Key, GetBitMask(key.Key), other.Key, GetBitMask(other.Key), cost.neededKeys, cost.cost)); 
+					keyDistances[key.Key].Add(new KeyDistance(key.Key, GetBitMask(key.Key), other.Key, GetBitMask(other.Key), cost.neededKeys, cost.cost));
 				}
 			}
 			var plCosts = d.Map(player);
@@ -73,7 +71,7 @@ namespace Year2019.Day18 {
 			minCost = long.MaxValue;
 			var possibleSteps = keyDistances['@'].Where(x => x.neededKeys == 0).ToList();
 			foreach (var st in possibleSteps) {
-				Traverse(st.distance, 0, '@', st.key2);
+				Traverse(st.distance, 0, st.key2);
 			}
 			Console.WriteLine($"Finished in {totalIterations} iterations");
 			return minCost;
@@ -93,7 +91,7 @@ namespace Year2019.Day18 {
 			return r;
 		}
 
-		private void Traverse(long steps, long keysFound, char keyFrom, char keyTo) {
+		private void Traverse(long steps, long keysFound, char keyTo) {
 			if (steps > minCost) return;
 			long newKeys = keysFound;
 			newKeys |= (1u << keyTo - 'a');
@@ -111,20 +109,18 @@ namespace Year2019.Day18 {
 				}
 				return;
 			}
-
-
 			var possibleSteps = keyDistances[keyTo].Where(x =>
-				(x.key2bits & ~newKeys) != 0		  //is a key we don't have yet
+				(x.key2bits & ~newKeys) != 0          //is a key we don't have yet
 				&& ((x.neededKeys & ~newKeys) == 0)); //we have all prerequisite keys
 
 			foreach (var st in possibleSteps.OrderBy(x => x.distance)) {
-				Traverse(steps + st.distance, newKeys, keyTo, st.key2);
+				Traverse(steps + st.distance, newKeys, st.key2);
 			}
 			totalIterations++;
 		}
 
 		long totalIterations = 0;
-			
+
 		public class Dijkstra18(char[,] input) {
 			int width, height;
 			HashSet<Vec> dirty;
@@ -196,10 +192,122 @@ namespace Year2019.Day18 {
 			}
 		}
 
-		protected override long SolvePart2() {
-			long result = 0;
+		Dictionary<char, Vec> players;
+		protected override void ReadInputPart2(string fileName) {
+			ReadInputPart1(fileName);
+			//@#@
+			//###
+			//@#@
+			input[player.x - 1, player.y - 1] = '1';
+			input[player.x, player.y - 1] = '#';
+			input[player.x + 1, player.y - 1] = '2';
 
-			return result;
+			input[player.x - 1, player.y] = '#';
+			input[player.x, player.y] = '#';
+			input[player.x + 1, player.y] = '#';
+
+			input[player.x - 1, player.y + 1] = '3';
+			input[player.x, player.y + 1] = '#';
+			input[player.x + 1, player.y + 1] = '4';
+			players = new Dictionary<char, Vec>();
+			players.Add('1', new Vec(player.x - 1, player.y - 1));
+			players.Add('2', new Vec(player.x + 1, player.y - 1));
+			players.Add('3', new Vec(player.x - 1, player.y + 1));
+			players.Add('4', new Vec(player.x + 1, player.y + 1));
+			//Debug();
+		}
+
+		private void Debug() {
+			Console.Clear();
+			for (long y = 0; y < height; y++) {
+				for (long x = 0; x < width; x++) {
+					Console.Write(input[x, y]);
+				}
+				Console.WriteLine();
+			}
+			Console.ForegroundColor = ConsoleColor.White;
+		}
+
+		private void TraversePart2(long steps, long keysFound, char pl1pos, char pl2pos, char pl3pos, char pl4pos, char keyFrom, char keyTo) {
+			if (steps > minCost) return;
+			long newKeys = keysFound;
+			newKeys |= (1u << keyTo - 'a');
+			long prevCost;
+			var posKey = (keys[keyTo], newKeys);
+			if (!minimumCosts.TryGetValue(posKey, out prevCost)) {
+				minimumCosts.Add(posKey, steps);
+			} else if (prevCost < steps) {
+				return;
+			}
+			if ((allKeys & ~newKeys) == 0) {
+				if (steps < minCost) {
+					minCost = steps;
+					Console.WriteLine($"Found current best {steps} in {sw.ElapsedMilliseconds} ms");
+				}
+				return;
+			}
+			var possibleSteps = keyDistances[pl1pos].Where(x =>
+				(x.key2bits & ~newKeys) != 0
+				&& ((x.neededKeys & ~newKeys) == 0));
+			foreach (var st in possibleSteps.OrderBy(x => x.distance)) {
+				TraversePart2(steps + st.distance, newKeys, st.key2, pl2pos, pl3pos, pl4pos, pl1pos, st.key2);
+			}
+			possibleSteps = keyDistances[pl2pos].Where(x =>
+				(x.key2bits & ~newKeys) != 0
+				&& ((x.neededKeys & ~newKeys) == 0));
+			foreach (var st in possibleSteps.OrderBy(x => x.distance)) {
+				TraversePart2(steps + st.distance, newKeys, pl1pos, st.key2, pl3pos, pl4pos, pl1pos, st.key2);
+			}
+			possibleSteps = keyDistances[pl3pos].Where(x =>
+				(x.key2bits & ~newKeys) != 0
+				&& ((x.neededKeys & ~newKeys) == 0));
+			foreach (var st in possibleSteps.OrderBy(x => x.distance)) {
+				TraversePart2(steps + st.distance, newKeys, pl1pos, pl2pos, st.key2, pl4pos, pl1pos, st.key2);
+			}
+			possibleSteps = keyDistances[pl4pos].Where(x =>
+				(x.key2bits & ~newKeys) != 0
+				&& ((x.neededKeys & ~newKeys) == 0));
+			foreach (var st in possibleSteps.OrderBy(x => x.distance)) {
+				TraversePart2(steps + st.distance, newKeys, pl1pos, pl2pos, pl3pos, st.key2, pl1pos, st.key2);
+			}
+			totalIterations++;
+		}
+
+		protected override long SolvePart2() {
+			keyDistances = new();
+			minimumCosts = new();
+			sw = Stopwatch.StartNew();
+			var d = new Dijkstra18(input);
+			var allObj = keys.Union(players);
+			foreach (var key in allObj) {
+				var costs = d.Map(key.Value);
+				keyDistances.Add(key.Key, new List<KeyDistance>());
+				foreach (var other in allObj.Where(x => x.Key != key.Key && !char.IsNumber(x.Key))) {
+					var cost = costs[other.Value.x, other.Value.y];
+					if (cost.cost != long.MaxValue) {
+						keyDistances[key.Key].Add(new KeyDistance(key.Key, GetBitMask(key.Key), other.Key, GetBitMask(other.Key), cost.neededKeys, cost.cost));
+					}
+				}
+			}
+			minCost = long.MaxValue;
+			var possibleSteps = keyDistances['1'].Where(x => x.neededKeys == 0).ToList();
+			foreach (var st in possibleSteps) {
+				TraversePart2(st.distance, 0, st.key2, '2', '3', '4', '1', st.key2);
+			}
+			possibleSteps = keyDistances['2'].Where(x => x.neededKeys == 0).ToList();
+			foreach (var st in possibleSteps) {
+				TraversePart2(st.distance, 0, '1', st.key2, '3', '4', '2', st.key2);
+			}
+			possibleSteps = keyDistances['3'].Where(x => x.neededKeys == 0).ToList();
+			foreach (var st in possibleSteps) {
+				TraversePart2(st.distance, 0, '1', '2', st.key2, '4', '3', st.key2);
+			}
+			possibleSteps = keyDistances['4'].Where(x => x.neededKeys == 0).ToList();
+			foreach (var st in possibleSteps) {
+				TraversePart2(st.distance, 0, '1', '2', '3', st.key2, '4', st.key2);
+			}
+			Console.WriteLine($"Finished in {totalIterations} iterations");
+			return minCost;
 		}
 	}
 }
