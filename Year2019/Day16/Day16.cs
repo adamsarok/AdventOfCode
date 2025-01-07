7f58c9f9bf5b800870a20d0e2b8383073930e081
Ádám Sarok <sarok.adam@gmail.com>
2025-01-07 19:31:31 +0100

    feat: 2019 day 16

--- Year2019/Day16/Day16.cs
+++ Year2019/Day16/Day16.cs
@@ -12,6 +12,7 @@ namespace Year2019.Day16 {
 		int[] input;
 		int[][] muls;
 		protected override void ReadInputPart1(string fileName) {
+			base.ReadInputPart1(fileName);
 			var l = File.ReadAllLines(fileName)[0];
 			input = new int[l.Length];
 			muls = new int[l.Length][];
@@ -48,6 +49,7 @@ namespace Year2019.Day16 {
 		int length;
 		int totalLength;
 		protected override void ReadInputPart2(string fileName) {
+			base.ReadInputPart2(fileName);
 			var l = File.ReadAllLines(fileName)[0];
 			offset = int.Parse(l.Substring(0, 7));
 			int multiply = 10000;
@@ -60,13 +62,16 @@ namespace Year2019.Day16 {
 				temp[i] = (int)(l[i] - '0');
 			}
 			input = Enumerable.Repeat(temp, multiply).SelectMany(x => x).Skip(offset).ToArray();
-			//with the offset we can ignore 90% of the characters - however that is still a lot to calculate
+			//with the offset we can ignore 90% of the characters (all multipliers zero before offset)
 
-			//for (int i = 0; i < length; i++) {
-			//	var p = (Enumerable.Repeat(GetPattern(i), (l.Length * multiply) / ((i + 1) * 4) + 1).SelectMany(x => x)).Skip(1 + offset).ToArray();
-			//	muls[i] = p; //this is bad but works
-			//}
+	
 		}
+
+		
+
+		record MultiplierRange(int from, int to, int multiplier);
+
+		private List<MultiplierRange> multiplierRanges;
 
 		protected override long SolvePart1() {
 			long result = 0;
@@ -92,15 +97,27 @@ namespace Year2019.Day16 {
 			return temp;
 		}
 
-		private int[] IteratePart2(int[] input) {
+		//private int[] IteratePart2(int[] input) {
+		//	var temp = new int[input.Length];
+		//	for (int i = 0; i < input.Length; i++) {
+		//		long acc = 0;
+		//		for (int j = i; j < input.Length; j++) {
+		//			acc += input[j]; //we only get 0 and 1 after the offset and the 1-s start at j
+		//		}
+		//		temp[i] = (int)(Math.Abs(acc) % 10);
+		//	}
+		//	return temp;
+		//}
+
+		//ohhhh its a triangle, we only need to start from the end and sum up...
+		//1*6 1*7 1*8
+		//0*6 1*7 1*8
+		//0*6 0*7 1*8
+		private int[] IteratePart2Better(int[] input) {
 			var temp = new int[input.Length];
-			for (int i = 0; i < input.Length; i++) {
-				long acc = 0;
-				var p = (Enumerable.Repeat(GetPattern(i), totalLength / ((i + 1) * 4) + 1).SelectMany(x => x)).Skip(1 + offset).ToArray();
-				for (int j = 0; j < input.Length; j++) {
-					acc += input[j] * p[j];
-					//Console.WriteLine($"{i}:{j}:->{input[j]}*{muls[i][j]}");
-				}
+			long acc = 0;
+			for (int i = input.Length - 1; i >= 0; i--) {
+				acc += input[i];
 				temp[i] = (int)(Math.Abs(acc) % 10);
 			}
 			return temp;
@@ -109,10 +126,10 @@ namespace Year2019.Day16 {
 		protected override long SolvePart2() {
 			long result = 0;
 			for (int i = 0; i < 100; i++) {
-				var temp = IteratePart2(input);
+				var temp = IteratePart2Better(input);
 				input = temp;
 			}
-			for (int i = 0; i < 8; i++) result += input[i] * (int)Math.Pow(10, 7 - i); //todo this is not it!
+			for (int i = 0; i < 8; i++) result += input[i] * (int)Math.Pow(10, 7 - i);
 			return result;
 		}
 	}
